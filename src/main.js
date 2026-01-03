7cb5502396e2f2d918f6a685db1d9414aa2bb6f1
Paul Tang <p-tang@unext.jp>
2026-01-03 13:58:25 +0800

    Update PWA

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./style.css";
+import { registerSW } from "virtual:pwa-register";
+
 
 createApp(App).mount("#app");
+registerSW({ immediate: true });
+
